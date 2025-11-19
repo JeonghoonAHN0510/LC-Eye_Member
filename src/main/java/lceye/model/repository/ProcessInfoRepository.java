57cdedeb19e29d01c64f530a329fc8a6b62f84d6
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-19 21:35:49 +0900

    build : Member와 관련없는 파일 삭제

--- src/main/java/lceye/model/repository/ProcessInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package lceye.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import lceye.model.entity.ProcessInfoEntity;
-
-public interface ProcessInfoRepository extends JpaRepository<ProcessInfoEntity,Integer> {
-
-}// interface end
--- src/main/java/lceye/model/repository/UnitsRepository.java
+++
@@ -1,8 +0,0 @@
-package lceye.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import lceye.model.entity.UnitsEntity;
-
-public interface UnitsRepository extends JpaRepository<UnitsEntity, Integer> {
-}
